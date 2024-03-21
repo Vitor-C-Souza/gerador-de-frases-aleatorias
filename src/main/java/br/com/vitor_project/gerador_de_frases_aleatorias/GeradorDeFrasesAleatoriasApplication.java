e53a4b86213ebd4dabf5437f4363867199a181af
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-03-21 18:26:36 -0300

    Inicio do projeto de gerador de frases aleatorias

---
+++ src/main/java/br/com/vitor_project/gerador_de_frases_aleatorias/GeradorDeFrasesAleatoriasApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitor_project.gerador_de_frases_aleatorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorDeFrasesAleatoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorDeFrasesAleatoriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vitor_project/gerador_de_frases_aleatorias/GeradorDeFrasesAleatoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitor_project.gerador_de_frases_aleatorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorDeFrasesAleatoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
